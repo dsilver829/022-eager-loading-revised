8972e3356bfb268587b16b4c8dbabc929475dc4a
David Silver <dsilver@candidatemetrics.com>
2013-03-21 13:47:12 -0700

    add join

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.order("name").includes(:category)
+    @products = Product.order("name").joins(:category).select("products.*, categories.name as category_name")
   end
 
   def show
